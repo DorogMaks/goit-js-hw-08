c6b94c1c97423146cec5d74842812b70fd61c614
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-16 20:17:13 +0300

    update task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryListMarkUp = createGalleryList(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryListMarkUp);
+
+function createGalleryList(imagesArray) {
+  return imagesArray
+    .map(({ original, preview, description }) => {
+      return `        
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                title="${description}"
+            />
+        </a>
+        `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  scrollZoom: false,
+});
