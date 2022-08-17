c99cf62a4f63a81e6f670290829bf6bda00c58fc
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-17 19:12:21 +0300

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const PLAYBACK_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(evt => {
+    localStorage.setItem(PLAYBACK_KEY, evt.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(PLAYBACK_KEY))
+  .catch(function (error) {
+    console.error(error);
+  });
