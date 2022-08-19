14fa0fc0526c1ba55f4dfe80d5beab61e9c60bd4
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-19 15:12:18 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+email.addEventListener('input', onEmailInput);
+message.addEventListener('input', onTextareaInput);
+
+populateMessageOutput();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onEmailInput(evt) {}
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateMessageOutput() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    message.value = savedMessage;
+  }
+}
